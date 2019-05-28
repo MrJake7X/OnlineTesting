93a64d44215d33ea22bc6358f3041fc2e9f06e8e
MrJake7X <alexreverter7@gmail.com>
2019-05-28 15:34:19 +0200

    Added Killer prefab and filter to spawn Killer or Survivor

--- OnlineTest/Assets/Scripts/NetworkCallbacks.cs
+++ OnlineTest/Assets/Scripts/NetworkCallbacks.cs
@@ -1,12 +1,22 @@
 ﻿using UnityEngine;
 
+
+// IMPORTANT, THIS IS CALLED AUTOMATICATLLY IF THE NEXT LINE IT'S WRITED, AND EXECUTE THE DIFERENTS CALLBACKS DEPENDING FROM SERVER/CLIENT ACTION
+
 [BoltGlobalBehaviour]
 public class NetworkCallbacks : Bolt.GlobalEventListener
 {
     public override void SceneLoadLocalDone(string scene)
     {
-        var spawnPosition = new Vector3(Random.Range(-8, 8), 0, Random.Range(-8,8));
+        var spawnPosition = new Vector3(Random.Range(-8, 8), 0, 0);
 
-        BoltNetwork.Instantiate(BoltPrefabs.Survivor, spawnPosition, Quaternion.identity);
+        if (BoltNetwork.IsServer)
+        {
+            BoltNetwork.Instantiate(BoltPrefabs.Killer, spawnPosition, Quaternion.identity);
+        }
+        else
+        {
+            BoltNetwork.Instantiate(BoltPrefabs.Survivor, spawnPosition, Quaternion.identity);
+        }
     }
 }
